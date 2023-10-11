be5f256f62c3c7a0a8ba3fda5007c16e1ed9bfe0
Yudiz Solutions Ltd <yudiz@Yudizs-Mac-mini-8.local>
2023-10-11 14:55:04 +0530

    Revert "Golang fiber api"
    This reverts commit 26c5335c0efd9a79fb015147fea971af6bffcef2.

--- services/jwt/jwt.struct.go
+++
@@ -1,8 +0,0 @@
-package jwt
-
-import "github.com/golang-jwt/jwt/v5"
-
-type CustomJwtClaims struct {
-	ID uint `json:"user_id"`
-	jwt.RegisteredClaims
-}
--- services/user/userAuth.struct.go
+++
@@ -1,9 +0,0 @@
-package user
-
-type (
-	UserSignUp struct {
-		Username string `json:"username" validate:"required,min=2"`
-		Email    string `json:"email" validate:"required,email"`
-		Password string `json:"password" validate:"required,min=6"`
-	}
-)
